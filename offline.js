72f401a1223c9509a613edfd132861808e3c27e9
Nityam Gandhi <73809626+sudo-charg3@users.noreply.github.com>
2024-08-14 08:33:27 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1638864684,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemy-sheet1.png",
+		"images/enemy-sheet2.png",
+		"images/enemy-sheet3.png",
+		"images/player-sheet0.png",
+		"images/bouncewalls-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/gameoveroverlay-sheet0.png",
+		"images/blasteffect.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
